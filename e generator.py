c5431f616811d3390d0a39d05fd685455da66c0e
AnarchoTak <54296988+takplusplus@users.noreply.github.com>
2021-08-11 06:14:34 -0400

    e converter becomes only a e converter
    doesnt actually convert only e. it just puts whatever you tell it as capital. 
    i added the abilty for it to check if its actually e so now it doesn't just tell you its not e
    and then capitalize whatever you type anyay, practcinng if else statments. still getting the hang of them

--- e generator.py
+++ e generator.py
@@ -1,4 +1,10 @@
+#plugs in whatever you type into this variable
 econverter = raw_input("make e into ITS FINAL FORM!")
-if not "e":
-    print("thats not e...you infadel")
-print("your e has become:" + econverter.upper())
+#this "e" probably doesn't even need a variable but i am
+#stoopid
+estore = "e"
+#again i could of just commpared it with a normal string?
+if econverter != estore:
+    print("thats not e...you silly")
+else:
+    print("your e has become:" + econverter.upper())
