f221cc6df21de60ddf6ea5e9059818d44d8923a8
AnarchoTak <54296988+takplusplus@users.noreply.github.com>
2021-08-11 06:00:18 -0400

    Add files via upload

---
+++ e generator.py
@@ -0,0 +1,4 @@
+econverter = raw_input("make e into ITS FINAL FORM!")
+if not "e":
+    print("thats not e...you infadel")
+print("your e has become:" + econverter.upper())
---
+++ simple check.py
@@ -0,0 +1,13 @@
+### i dont know if i need this lmao i am a noob this was
+### at the top of my head. : D
+import sys
+import time
+point = 0
+wordtracker = "poop"
+userword = raw_input()
+if userword == wordtracker:
+    point += 1
+    time.sleep(5)
+    print("your point is")
+    print(point)
+else: print("hooray you didnt say the bad word")
